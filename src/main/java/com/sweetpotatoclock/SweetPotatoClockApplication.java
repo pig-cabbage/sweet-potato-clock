78ad9efce2e632fb4a1669158f471fa17eb2b856
pig-cabbage <2089319261@qq.com>
2019-04-21 14:27:50 +0800

    init commit

---
+++ src/main/java/com/sweetpotatoclock/SweetPotatoClockApplication.java
@@ -0,0 +1,13 @@
+package com.sweetpotatoclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetPotatoClockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetPotatoClockApplication.class, args);
+    }
+
+}
