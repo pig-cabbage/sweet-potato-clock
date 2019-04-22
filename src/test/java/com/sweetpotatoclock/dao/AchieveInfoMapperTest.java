6640e51693e8e8d7433b6059b086199f4c95601b
pig-cabbage <2089319261@qq.com>
2019-04-22 17:36:04 +0800

    li_back

---
+++ src/test/java/com/sweetpotatoclock/dao/AchieveInfoMapperTest.java
@@ -0,0 +1,28 @@
+package com.sweetpotatoclock.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AchieveInfoMapperTest {
+
+    @Test
+    public void deleteByPrimaryKey() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+    }
+
+    @Test
+    public void selectAll() {
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+    }
+}
